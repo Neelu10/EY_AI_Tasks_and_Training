a1039a05af950914dfd055bca98f142bc0d3120b
Neelu10 <neelamp2512@gmail.com>
2025-09-22 14:44:51 +0530

    updated commit

---
+++ 22_9_25/daily activities/nump_prac2.py
@@ -0,0 +1,13 @@
+import numpy as np
+marks=np.array([10,30,50,90])
+
+print('Max marks:',max(marks))
+print('Min marks:',min(marks))
+print('Average marks:',np.mean(marks))
+
+
+data = np.array([1,2,3,4,5])
+print("first three elements",data[:3])
+print("reversed",data[::-1])
+print("sum",np.sum(data))
+print("std deviation",np.std(data))
